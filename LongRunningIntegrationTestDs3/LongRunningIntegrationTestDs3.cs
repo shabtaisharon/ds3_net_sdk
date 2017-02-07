1789b521942630e42a7f3733aecfa3b610314a3e
Sharon Shabtai <sharons@spectralogic.com>
2017-02-07 11:41:45 -0700

    .

--- LongRunningIntegrationTestDs3/LongRunningIntegrationTestDs3.cs
+++ LongRunningIntegrationTestDs3/LongRunningIntegrationTestDs3.cs
@@ -67,7 +67,7 @@ namespace LongRunningIntegrationTestDs3
         public void PutLargeNumberOfObjects()
         {
             const string bucketName = "PutLargeNumberOfObjects";
-            const int numberOfObjects = 25000;
+            const int numberOfObjects = 20000;
 
             try
             {
